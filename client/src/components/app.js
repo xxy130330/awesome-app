751dea61432fdff84489505f39ba372ea3111586
xxy130330 <yangxue0708@gmail.com>
2017-12-22 12:35:54 -0800

    finished react setup

---
+++ client/src/components/app.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Link, Route} from 'react-router-dom';
+import Home from './home';
+import GetData from './get_data';
+
+// import '../assets/css/app.css';
+// import logo from '../assets/images/logo.svg';
+
+const App = () => (
+    <div>
+        <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/get-data">Get Data</Link></li>
+        </ul>
+
+        <Route exact path="/" component = {Home}/>
+        <Route path="/get-data" component={GetData}/>
+    </div>
+);
+
+export default App;
---
+++ client/src/components/get_data.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class GetData extends Component{
+
+    render(){
+        return(
+            <div>
+                <h1>This is get Data!</h1>
+            </div>
+        )
+    }
+}
+
+export default GetData;
